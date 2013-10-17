85433cc4175e4954a82457ac223ee17838899e9e
Jay H <mobeets@gmail.com>
2013-10-17 15:23:10 -0500

    dropdown instead of slider

--- d3.bar.js
+++ d3.bar.js
@@ -1,6 +1,46 @@
 // https://github.com/mbostock/d3/wiki/API-Reference
+var data_filename = 'data.csv';
+var x_axis_name = 'pattern';
+var y_axis_name = 'frequency';
+var color_axis_name = 'word length';
 
-var d3_slider = d3.slider().value(0);
+d3.selectAll('#dropdown_name').text('Filter by ' + color_axis_name + ':');
+d3.selectAll('#key_name').text(x_axis_name);
+d3.selectAll('#value_name').text(y_axis_name);
+d3.selectAll('#group_name').text(color_axis_name);
+
+function init_data(d) {
+  d.group = +d.group;
+  d.val = +d.val;
+}
+
+function get_key(d) {
+  return d.key;
+}
+
+function get_value(d) {
+  return d.val;
+}
+
+function get_group(d) {
+  return d.group;
+}
+
+function sort_data_by_value(a, b) {
+  return b.val - a.val;
+}
+
+function sort_data_by_key(a, b) {
+  return d3.ascending(a.key, b.key);
+}
+
+function sort_data_by_group(a, b) {
+  return b.group - a.group;
+}
+
+function sort_group(a, b) {
+  return a - b; // n.b. need to change if group values are not integers
+}
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 600 - margin.left - margin.right,
@@ -23,9 +63,7 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(formatter);
 
-var color = d3.scale.ordinal()
-    // .domain(["foo", "bar", "baz"])
-    .range(colorbrewer.PuOr[9]);
+var color = d3.scale.ordinal();
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -33,34 +71,75 @@ var svg = d3.select("#chart").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var radio_val = 'value';
+var all_data;
 var cur_data;
-d3.csv("data.csv", function(data) {
+d3.csv(data_filename, function(data) {
+  all_data = data;
+  all_data.forEach(init_data);
 
-  data.forEach(init_data);
-  cur_data = filter_data(data, 0);
-  color.domain([d3.min(data, all_keys), d3.max(data, all_keys)]);
+  set_colors_by_group(colorbrewer.Greens, 3, 9);
+  init_axes();
+  init_dropdown();
+  filter_data(-1);
+  init_radios();
+});
 
-  d3_slider.on("slide", function(evt, value) {
-    cur_data = filter_data(data, value);
-    // change_sorting(false);
-  })
+function sorted_set(data, getter, sorter) {
+  group_list = [];
+  for (i in data){
+    group = getter(data[i]);
+    if (group_list.indexOf(group) == -1){
+      group_list.push(group);
+    }
+  }
+  return group_list.sort(sorter);
+}
 
-  max_val = d3.max(data, all_keys);
-  d3.select('#slider').call(
-    d3_slider.axis( d3.svg.axis().ticks(max_val) ).min(0).max(max_val).step(1)
-  );
+function set_colors_by_group(colors_lookup, min_ncolors, max_ncolors) {
+  group_list = sorted_set(all_data, get_group, sort_group);
+  ncolors = group_list.length;
+  if (ncolors < min_ncolors || ncolors > max_ncolors){
+    d3.selectAll('#warnings').text('WARNING: There are ' + ncolors + ' groups in your data, but only 9 colors.');
+    ncolors = min(max(min_ncolors, ncolors), max_ncolors);
+  }
+  color.domain(group_list)
+    .range(colors_lookup[ncolors]);
+}
 
+function init_dropdown() {
+  group_list = sorted_set(all_data, get_group, sort_group);
+  group_list.unshift(-1); // for choosing all
+
+  d3.select("#dropdown").append("select")
+      .on("change", change_dropdown)
+    .selectAll("option").data(group_list).enter()
+      .append("option")
+        .attr("value", function(d){return d;}) /* Optional */
+        .text(function(d){return d == -1 ? 'All': d;});
+}
+
+function change_dropdown() {
+  ind = this.selectedIndex;
+  val = this.options[ind].value;
+  filter_data(val);
+}
+
+function init_radios() {
+  d3.selectAll("input")
+    .on("change", function() { radio_val = this.value; return resort_data(radio_val); });
+}
+
+function init_axes() {
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
     .append("text")
-      // .attr("transform", "rotate(-90)")
       .attr("x", 6)
       .attr("y", 16)
-      // .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Pattern");
+      .text(x_axis_name);
 
   svg.append("g")
       .attr("class", "y axis")
@@ -70,52 +149,25 @@ d3.csv("data.csv", function(data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Frequency");
-
-  // d3.select("#button").on("click", check_sorting);
-  d3.selectAll("input").on("change", check_sorting);
-
-});
-
-function check_sorting() {
-    // sort_method = $('#button-wrapper').data('sort-method');
-    // to_by_value = sort_method == 'frequency';
-    val = this.value;
-    // alert(val);
-    change_sorting(this.value == 'freq');
-
-    // new_sort_method = 'frequency';
-    // if (to_by_value)
-    //   new_sort_method = 'group';
-    
-    // $('#button-wrapper').attr('data-sort-method', new_sort_method);
-    // $('#button').html('Sort by ' + new_sort_method);
+      .text(y_axis_name);
 }
 
-function change_sorting(to_by_value) {
-  // Copy-on-write since tweens are evaluated after a delay.
-  sorter = sort_by_name;
-  if (val == 'freq')
-    sorter = sort_by_value;
-  else if (val == 'nchars')
-    sorter = sort_by_group;
+function resort_data(val) {
+  sorter = sort_data_by_key;
+  if (val == 'value')
+    sorter = sort_data_by_value;
+  else if (val == 'group')
+    sorter = sort_data_by_group;
 
-  var x0 = x.domain(cur_data.sort(sorter)
-      .map(function(d) { return d.pattern; }))
-      .copy();
-  // var x0 = x.domain(cur_data.sort(to_by_value
-  // // var x0 = x.domain(cur_data.sort(this.checked
-  //     ? sort_by_value
-  //     : sort_by_name)
-  //     .map(function(d) { return d.pattern; }))
-  //     .copy();
+  // Copy-on-write since tweens are evaluated after a delay.
+  var x0 = x.domain(cur_data.sort(sorter).map(get_key)).copy();
 
   var transition = svg.transition().duration(750),
       delay = function(d, i) { return i * 50; };
 
   transition.selectAll(".bar")
       .delay(delay)
-      .attr("x", function(d) { return x0(d.pattern); });
+      .attr("x", function(d) { return x0(get_key(d)); });
 
   transition.select(".x.axis")
       .call(xAxis)
@@ -123,43 +175,14 @@ function change_sorting(to_by_value) {
       .delay(delay);
 }
 
-function init_data(d) {
-  d.nchars = +d.nchars;
-  d.freq = +d.freq;
-}
-
-function all_names(d) {
-  return d.pattern;
-}
-
-function all_values(d) {
-  return d.freq;
-}
-
-function all_keys(d) {
-  return d.nchars;
-}
-
-function sort_by_value(a, b) {
-  return b.freq - a.freq;
-}
-
-function sort_by_name(a, b) {
-  return d3.ascending(a.pattern, b.pattern);
-}
-
-function sort_by_group(a, b) {
-  return b.nchars - a.nchars;
-}
-
-function filter_data(data, val) {
-  cur_data = data.filter(function(d) { return d.nchars == val || val == 0; });
-  x.domain(cur_data.map(all_names));
+function filter_data(val) {
+  cur_data = all_data.filter(function(d) { return get_group(d) == val || val == -1; });
+  x.domain(cur_data.map(get_key));
   svg.select(".x.axis").transition()
       .duration(1000)
       .call(xAxis);
 
-  y.domain([0, d3.max(cur_data, all_values)]);
+  y.domain([0, d3.max(cur_data, get_value)]);
   svg.select(".y.axis").transition()
       .duration(1000)
       .call(yAxis);
@@ -172,18 +195,15 @@ function filter_data(data, val) {
 
   bar.transition()
     .duration(1000)
-    .attr("test1", function(d) { return d.nchars; })
-    .attr("test2", function(d) { return color(d.nchars); })
-    .style("fill", function(d) { return color(d.nchars); })
-    .attr("x", function(d) { return x(d.pattern); })
+    .style("fill", function(d) { return color(get_group(d)); })
+    .attr("x", function(d) { return x(get_key(d)); })
     .attr("width", x.rangeBand())
-    .attr("y", function(d) { return y(d.freq); })
-    .attr("height", function(d) { return height - y(d.freq); });
+    .attr("y", function(d) { return y(get_value(d)); })
+    .attr("height", function(d) { return height - y(get_value(d)); });
   
   bar.exit().transition()
     .duration(100)
     .remove();
 
-
-  return cur_data;
+  // resort_data(radio_val);
 }
